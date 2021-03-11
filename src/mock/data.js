8645147b833c91ad8e43663a8416793384ffe3de
Matt de la Rosa <mattthematt7@gmail.com>
2021-03-11 00:26:33 -0500

    adding files

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,67 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Matthew de la Rosa | GT CS 22', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my portfolio!', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Hello, my name is ',
+  name: 'Matthew',
+  subtitle: 'CS developer and student',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'Headshot.jpg',
+  paragraphOne:
+    'I am a student attending Georgia Institute of Technology as a computer science major. I began my interest in computers at a young age by playing online video games. This piqued my interest and in high school I began taking some coding classes. After high school when I first began attending college, I wanted to focus on cyber security and one day make my own anti-virus software. After taking some more coding classes my interests shifted more into the UI aspect of computer science. I then changed my concentrations to be more focused in app development and UI. Since then, I love creating apps and making user interfaces that are visually aesthetic as well as easy to navigate. ',
+  paragraphTwo:
+    'In the past I have done a lot of object-oriented programming focused on data structures and algorithms. I’ve had multiple group projects that spanned the course of a semester or more to put together a functional app that has many layers and depth. Some of the C work that I’ve done has consisted of creating games for the GBA in a variety of modes as well as implementing sprites and different states.',
+  paragraphThree: 'My preferred languages to use are Python, Java, and C.',
+  resume:
+    'https://docs.google.com/document/d/1JOoGVHvlZRnSDVLovOzmVuJcGCFLERD4hYKURC_aGz0/edit?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'literacyapppic.jpg',
+    title: '3rd Grade Literacy App',
+    info: 'This is a literacy app used to help 3rd graders learn basic word structures.',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/mattthematt76-crypto/3rd-grade-literacy-app', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'spaceinvaderspic.jpg',
+    title: 'Space Invaders',
+    info: 'This is a recreation of the classic game Space Invaders.',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/mattthematt76-crypto/Space-Invaders', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'ast.jpg',
+    title: 'Asteroids',
+    info: 'This is a recreation of the classic game Asteroids.',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/mattthematt76-crypto/Asteroids', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
-  btn: '',
-  email: '',
+  btn: 'Email',
+  email: 'mrosa9@gatech.edu',
 };
 
 // FOOTER DATA
@@ -67,28 +70,13 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'linkedin',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/mattthematt76-crypto',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
